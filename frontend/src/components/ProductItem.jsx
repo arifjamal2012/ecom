2b8bac74f79177b2d591f93c1a794ebd667409a3
arif jamal <arif.jamal@squintmetrics.com>
2024-10-10 14:19:35 -0400

    first commit

---
+++ frontend/src/components/ProductItem.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom';
+import { ShopContext } from '../context/ShopContext'
+
+const ProductItem = ({id,image,name,price,}) => {
+    const {currency} = useContext(ShopContext);
+    // console.log(id,"hello");
+    return (
+        <Link className="text-gray-700 cursor-pointer" to={`/product/${id}`}>
+            <div className="overflow-hidden">
+                <img className="hover:scale-110 transition ease-in-out" src={image[0]} alt="products" />
+            </div>
+            <p className="pt-3 pb-1 text-sm">{name}</p>
+            <p className="text-sm font-medium">{currency}{price}</p>
+        </Link>
+    )
+}
+
+export default ProductItem
